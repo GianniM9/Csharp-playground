664fb6d6308ebb840e070aab39927c04f2aa1e77
GianniM9 <gianni.morana@protonmail.com>
2025-09-06 19:09:34 +0200

    feat(week02-level-02): add leap-years-extended my_attempt

---
+++ Challenges/Week_2/Level_2/Challenge_02_Leap_Years_Extended/my_attempt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a start year: ");
+        int startYear = int.Parse(Console.ReadLine());
+        Console.Write("Enter an end year: ");
+        int endYear = int.Parse(Console.ReadLine());
+        int found = 0;
+        Console.WriteLine($"Leap years between {startYear} and {endYear}:");
+        if (startYear > endYear)
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+        for (int year = startYear; year <= endYear; year++)
+        {
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+            {
+                int leapYear = year;
+                Console.WriteLine($"{leapYear}");
+                found++;  
+            }                             
+        }
+        if (found == 0)
+        {
+            Console.WriteLine($"No leap years found.");
+        }
+    }
+}
