f162fd2c0a2b9c9b2449d52a27d6f6614c1ac154
GianniM9 <gianni.morana@protonmail.com>
2025-09-06 16:10:51 +0200

    feat(week02-level-02): add number-classifier my_attempt

---
+++ Challenges/Week_2/Level_2/Challenge_01_Number_Classifier/my_attempt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number == 0)
+        {
+            Console.WriteLine("Zero");
+        }
+        else if (number > 0 && number % 2 == 0)
+        {
+            Console.WriteLine("Positive and Even");
+        }
+        else if (number > 0 && number % 2 != 0)
+        {
+            Console.WriteLine("Positive and Odd");
+        }
+        else if (number < 0 && number % 2 == 0)
+        {
+            Console.WriteLine("Negative and Even");
+        }
+        else if (number < 0 && number % 2 != 0)
+        {
+            Console.WriteLine("Negative and Odd");
+        }
+    }
+}
