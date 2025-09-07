e75e66937c0be2688c5411ce860b217c6960bd64
GianniM9 <gianni.morana@protonmail.com>
2025-09-07 20:08:00 +0200

    Challenges/Week_2/Level_2/Challenge_04_FizzBuzz/my_attempt.cs

--- Challenges/Week_2/Level_2/Challenge_04_FizzBuzz/my_attempt.cs
+++ Challenges/Week_2/Level_2/Challenge_04_FizzBuzz/my_attempt.cs
@@ -6,14 +6,18 @@ class Program
     {
         for (int number = 1; number <= 100; number++)
         {
-            if (number % 3 == 0)
+            if (number % 5 == 0 && number % 3 == 0)
             {
-                Console.WriteLine("Frizz");
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
             }
             else if (number % 5 == 0)
             {
                 Console.WriteLine("Buzz");
-            }
+            } 
             else
             {
                 Console.WriteLine($"{number}");
