8e2c7c43e0916df2ef02635c98da050ba0091a9f
GianniM9 <gianni.morana@protonmail.com>
2025-09-07 19:29:07 +0200

    Challenges/Week_2/Level_2/Challenge_03_Guess_the_number/my_attempt.cs

---
+++ Challenges/Week_2/Level_2/Challenge_03_Guess_the_number/my_attempt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var random = new Random();
+        int secretNumber = random.Next(1, 101);
+        int guessedNumber = -1;
+        int attempt = 0;
+        while (guessedNumber != secretNumber)
+        {
+            Console.Write("Guess the number: ");
+            guessedNumber = int.Parse(Console.ReadLine());
+            attempt++;
+            if (guessedNumber > secretNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guessedNumber < secretNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            
+            if (guessedNumber == secretNumber)
+            {
+                Console.WriteLine("Correct!");
+            }                                 
+        }
+        Console.WriteLine($"Number of attempts: {attempt}");
+    }
+}
