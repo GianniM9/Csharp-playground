56a5bf2c75ce5a49dde497b295e31ba06c0bd355
GianniM9 <gianni.morana@protonmail.com>
2025-09-08 18:57:09 +0200

    Challenges/Week_2/Level_2/Challenge_07_Prime_checker/my_attempt.cs

---
+++ Challenges/Week_2/Level_2/Challenge_07_Prime_checker/my_attempt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        bool isPrime = true;
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int n = 2; n <= (number - 1); n++)
+            {
+                if (number % n == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+                else if (number % 1 == 0 && number % n == 0)
+                {
+                    isPrime = true;
+                    break;
+                }
+            }
+        }
+        if (isPrime)
+        {
+            Console.WriteLine("Prime");
+        }
+        else
+        {
+            Console.WriteLine("Not Prime");
+        }
+    }
+}
