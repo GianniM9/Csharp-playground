d8cb97bbb0e80fbaf78ba7c48232b6836fe27449
GianniM9 <gianni.morana@protonmail.com>
2025-09-08 15:42:43 +0200

    commit all

---
+++ Challenges/Week_2/Level_2/Challenge_06_Tables_generator/my_attempt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        for (int multiplier = 1; multiplier <= 10; multiplier++)
+        {
+            int result = number * multiplier;
+            Console.WriteLine($"{number} x {multiplier} = {result}");
+        }
+    }
+}
